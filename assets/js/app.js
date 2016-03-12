c5ecf1f74c9155040ddb4744397c5a4b8fad7bda
Dominic Lear <dominic.lear@rmpenterprise.co.uk>
2016-03-12 20:47:40 +0000

    break apart content into partials. start responsive styling and fallbacks.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,82 +1,77 @@
-// app.js
-
-//(function() {
-//	// Doc ready
-//	$(function(){//
-
-//	});//
-
-//	// Normal stuff
-//	
-//})();
-
-// http://codepen.io/LukyVj/pen/yNwgrK
-
-// TOGGLE SECTIONS
-// querySelector, jQuery style
-var $ = function (selector) {
-  return document.querySelectorAll(selector);
-};
-
-
-// Define tabs, write down them classes
-var tabs = [
-  '.tabbed-section__selector-tab-1',
-  '.tabbed-section__selector-tab-2',
-  '.tabbed-section__selector-tab-3',
-  '.tabbed-section__selector-tab-4'
-];
-
-// Create the toggle function
-var toggleTab = function(element) {
-  var parent = element.parentNode;
-  
-  // Do things on click
-  $(element)[0].addEventListener('click', function(){
-    // Remove the active class on all tabs.
-    // climbing up the DOM tree with `parentNode` and target 
-    // the children ( the tabs ) with childNodes
-    console.log(this.parentNode.childNodes);
-    this.parentNode.childNodes[1].classList.remove('active');
-    this.parentNode.childNodes[3].classList.remove('active');
-    this.parentNode.childNodes[5].classList.remove('active');
-    this.parentNode.childNodes[7].classList.remove('active');
-
-    // Then, give `this` (the clicked tab), the active class
-    this.classList.add('active');
-    
-    // Check if the clicked tab contains the class of the 1 or 2
-    if(this.classList.contains('tabbed-section__selector-tab-1')) {
-      toggleClass(1, [2,3,4]);
-    }
-
-    if(this.classList.contains('tabbed-section__selector-tab-2')) {
-    	toggleClass(2, [1,3,4]);
-    }
-    
-    if(this.classList.contains('tabbed-section__selector-tab-3')) {
-    	toggleClass(3, [1,2,4]);
-    }
-
-    if(this.classList.contains('tabbed-section__selector-tab-4')) {
-    	toggleClass(4, [1,2,3]);
-    }
-
-  });
-};
-
-function toggleClass(id, excludes) {
-	$('.tabbed-section-' + id)[0].classList.remove('hidden');
-    $('.tabbed-section-' + id)[0].classList.add('visible');
-
-    for (var i = 0; i < excludes.length; i++) {
-		$('.tabbed-section-' + excludes[i])[0].classList.remove('visible');
-		$('.tabbed-section-' + excludes[i])[0].classList.add('hidden');    	
-    }
+//Modernizr.classlist = false;
+if (Modernizr.queryselector && Modernizr.classlist) {
+	tabatron2000();
+}
+else {
+	//document.getElementsByTagName('html')[0].className+=' no-classlist';
 }
 
-// Then finally, iterates through all tabs, to activate the 
-// tabs system.
-for (var i = tabs.length - 1; i >= 0; i--) {
-  toggleTab(tabs[i]);
+// Based on http://codepen.io/LukyVj/pen/yNwgrK
+function tabatron2000() {
+	// jQuery like query selector
+	var $ = function (selector) {
+		return document.querySelectorAll(selector);
+	};
+
+	// Define tabs
+	var tabs = [
+		'.tabbed-section__selector-tab-1',
+		'.tabbed-section__selector-tab-2',
+		'.tabbed-section__selector-tab-3',
+		'.tabbed-section__selector-tab-4'
+	];	
+
+	// Create the toggle function
+	var toggleTab = function(element) {
+		var parent = element.parentNode;
+		
+		// Do things on click
+		$(element)[0].addEventListener('click', function(e){
+			// Remove the active class on all tabs.
+			// climbing up the DOM tree with `parentNode` and target 
+			// the children ( the tabs ) with childNodes
+			e.preventDefault();
+			//console.log(this.parentNode.childNodes);
+			this.parentNode.childNodes[1].classList.remove('active');
+			this.parentNode.childNodes[3].classList.remove('active');
+			this.parentNode.childNodes[5].classList.remove('active');
+			this.parentNode.childNodes[7].classList.remove('active');
+
+			// Then, give `this` (the clicked tab), the active class
+			this.classList.add('active');
+			
+			// Check if the clicked tab contains the class of the 1 or 2
+			if(this.classList.contains('tabbed-section__selector-tab-1')) {
+				toggleClass(1, [2,3,4]);
+			}
+
+			if(this.classList.contains('tabbed-section__selector-tab-2')) {
+				toggleClass(2, [1,3,4]);
+			}
+			
+			if(this.classList.contains('tabbed-section__selector-tab-3')) {
+				toggleClass(3, [1,2,4]);
+			}
+
+			if(this.classList.contains('tabbed-section__selector-tab-4')) {
+				toggleClass(4, [1,2,3]);
+			}
+
+		});
+	};
+
+	function toggleClass(id, excludes) {
+		$('.tabbed-section-' + id)[0].classList.remove('hidden');
+		$('.tabbed-section-' + id)[0].classList.add('visible');
+
+		for (var i = 0; i < excludes.length; i++) {
+			$('.tabbed-section-' + excludes[i])[0].classList.remove('visible');
+			$('.tabbed-section-' + excludes[i])[0].classList.add('hidden');    	
+		}
+	}
+
+	for (var i = tabs.length - 1; i >= 0; i--) {
+		toggleTab(tabs[i]);
+	}
+
 }
