ac33cf914fa3d0d48a4fc00206f2273a1b2cc051
Dominic Lear <dominic.lear@rmpenterprise.co.uk>
2016-03-12 23:14:50 +0000

    rewrite tab js

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,57 +21,62 @@ function tabatron2000() {
 		'.tabbed-section__selector-tab-4'
 	];	
 
+	function init () {
+		for (var i = 0; i < tabs.length; i++) {
+			toggleTab(tabs[i]);
+		}
+	}
+
 	// Create the toggle function
 	var toggleTab = function(element) {
-		var parent = element.parentNode;
+		//var parent = element.parentNode;
 		
 		// Do things on click
 		$(element)[0].addEventListener('click', function(e){
-			// Remove the active class on all tabs.
-			// climbing up the DOM tree with `parentNode` and target 
-			// the children ( the tabs ) with childNodes
+
+			// Stop default anchor behaviour with #links
 			e.preventDefault();
-			//console.log(this.parentNode.childNodes);
-			this.parentNode.childNodes[1].classList.remove('active');
-			this.parentNode.childNodes[3].classList.remove('active');
-			this.parentNode.childNodes[5].classList.remove('active');
-			this.parentNode.childNodes[7].classList.remove('active');
 
-			// Then, give `this` (the clicked tab), the active class
+			// Set the click tab link as active
+			toggleTabLink();
 			this.classList.add('active');
-			
-			// Check if the clicked tab contains the class of the 1 or 2
-			if(this.classList.contains('tabbed-section__selector-tab-1')) {
-				toggleClass(1, [2,3,4]);
-			}
 
-			if(this.classList.contains('tabbed-section__selector-tab-2')) {
-				toggleClass(2, [1,3,4]);
-			}
-			
-			if(this.classList.contains('tabbed-section__selector-tab-3')) {
-				toggleClass(3, [1,2,4]);
-			}
-
-			if(this.classList.contains('tabbed-section__selector-tab-4')) {
-				toggleClass(4, [1,2,3]);
-			}
+			// Set the tab content as active
+			var tabId = this.getAttribute('data-tab-id');
+			toggleTabContent(tabId);
 
 		});
 	};
 
-	function toggleClass(id, excludes) {
-		$('.tabbed-section-' + id)[0].classList.remove('hidden');
-		$('.tabbed-section-' + id)[0].classList.add('visible');
-
-		for (var i = 0; i < excludes.length; i++) {
-			$('.tabbed-section-' + excludes[i])[0].classList.remove('visible');
-			$('.tabbed-section-' + excludes[i])[0].classList.add('hidden');    	
+	// Toggle Tab Links
+	function toggleTabLink() {
+		var tabSelectors = $('.tab-link');
+		for (var i = 0; i < tabSelectors.length; i++) {
+			tabSelectors[i].classList.remove('active');
 		}
 	}
 
-	for (var i = tabs.length - 1; i >= 0; i--) {
-		toggleTab(tabs[i]);
+	// Toggle Tab Content
+	function toggleTabContent(activeTab) {
+		// Get all the tabs
+		var tabs = $('.tab-content');
+
+		for (var i = 0; i < tabs.length; i++) {
+
+			// Get the tab index from the current tab
+			var tabId = tabs[i].getAttribute('data-tab');
+
+			// If it matches the active tab id from the link show it if not hide it
+			if (tabId == activeTab) {
+				tabs[i].classList.remove('hidden');
+				tabs[i].classList.add('visible');
+			} else {
+				tabs[i].classList.remove('visible');
+				tabs[i].classList.add('hidden');
+			}
+		}
+
 	}
 
+	init();
 }
